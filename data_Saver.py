45c781b8a802ca8668c71621ce6e2a84c4b0d4cc
Gogeishvili <gio.gogeishvili@gmail.com>
2024-09-15 19:11:38 +0400

    Fixed data saving bug

--- data_Saver.py
+++ data_Saver.py
@@ -6,13 +6,5 @@ class DataSaver:
 
     @staticmethod
     def save_data(data):
-        try:
-            with open(DataSaver.__file_name, "r") as json_file:
-                existing_data = json.load(json_file)
-        except FileNotFoundError:
-            existing_data = []
-
-        existing_data.extend(data)
-
         with open(DataSaver.__file_name, "w") as json_file:
-            json.dump(existing_data, json_file, indent=4)
+            json.dump(data, json_file, indent=4)
